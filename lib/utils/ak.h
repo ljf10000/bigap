3adc95ac40c402e1f7d03068baa26204f587222a
ljf10000 <l_j_f@163.com>
2016-02-18 17:06:28 +0800

    .

--- lib/utils/ak.h
+++ lib/utils/ak.h
@@ -248,6 +248,8 @@ __ak_get_value(char *key, char *value)
     * not digit string, try sys
     */
     sys = __ak_sys_idx(key);
+    os_println("__ak_get_value key=%s", key);
+    
     if (sys<0) {
         os_println("__ak_get_value default v=0x%x", __ak_debug_default);
         
