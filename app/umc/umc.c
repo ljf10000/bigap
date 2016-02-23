2c393601f2686c6d0254c41656d6c944235fee15
ljf10000 <l_j_f@163.com>
2016-02-23 12:32:57 +0800

    .

--- app/umc/umc.c
+++ app/umc/umc.c
@@ -275,7 +275,7 @@ cmd_tag(int argc, char *argv[])
     char *mac = argv[0];
 
     if (2!=argc || 3!=argc) {
-        __cli_argv_dump(println, argc, argv);
+        __cli_argv_dump(os_println, argc, argv);
         
         return -EFORMAT;
     }
