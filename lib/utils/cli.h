7cad802a93aaf36749f4e97bdf60b42c40dfc753
ljf10000 <l_j_f@163.com>
2016-02-23 16:36:47 +0800

    .

--- lib/utils/cli.h
+++ lib/utils/cli.h
@@ -96,17 +96,19 @@ cli_argv_handle(cli_table_t tables[], int count, int argc, char *argv[])
 #endif /* defined(__APP__) || defined(__BOOT__) */
 /******************************************************************************/
 #ifdef __APP__
+#ifndef cli_buffer_hsize
+#define cli_buffer_hsize        (sizeof(int) + 3*sizeof(uint32_t))
+#endif
+
+#ifndef CLI_BUFFER_SIZE
+#define CLI_BUFFER_SIZE         (256*1024 - cli_buffer_hsize - 1)
+#endif
+
 typedef struct {
     uint32_t len, r0, r1;
     int err;
     char buf[1+CLI_BUFFER_SIZE];
 } cli_buffer_t;
-
-#define cli_buffer_hsize            offsetof(cli_buffer_t, buf)
-
-#ifndef CLI_BUFFER_SIZE
-#define CLI_BUFFER_SIZE             (256*1024 - cli_buffer_hsize - 1)
-#endif
 
 #define DECLARE_FAKE_CLI_BUFFER     extern cli_buffer_t __THIS_CLI_BUFFER
 #define DECLARE_REAL_CLI_BUFFER     cli_buffer_t __THIS_CLI_BUFFER
