6668a90cb4bfeaa11c7ed622b7dad5a0359d1ce2
ljf10000 <l_j_f@163.com>
2016-03-04 17:51:52 +0800

    .

--- test/blob/blob.c
+++ test/blob/blob.c
@@ -39,10 +39,10 @@ put_somthing(char *name)
     blob_put_i64(bs, "i64", -1);
     blob_put_u64(bs, "u64", 64);
     blob_put_f64(bs, "f64", 64.64);
-    blob_put_string(bs, "string", "ccccccc");
-#if 0
+    blob_put_string(bs, "string", "sssssss");
+#if 1
     blob_put_binary(bs, "binary", "bbbbbbb", 7);
-    blob_sprintf(bs "sprintf", "%s-%d", "bbbbbbb", 7);
+    blob_put_sprintf(bs, "sprintf", "%s-%d", "bbbbbbb", 7);
 #endif
 
     if (name) {
