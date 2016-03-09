fa1458b5506b5b0b2a285f33af1e095c06ac1fba
ljf10000 <l_j_f@163.com>
2016-03-09 16:59:08 +0800

    .

--- app/duktape/module/js/stream.js
+++ app/duktape/module/js/stream.js
@@ -1,16 +1,5 @@
 #!/bin/js
 
 print("stream 1");
-function Stream(filename, mode, pipe = false) {
-	this.filename   = filename;
-	this.mode       = mode;
-	this.pipe       = pipe;
-
-	if (pipe) {
-		this.stream = __libc__.fopen(filename, mode);
-	} else {
-		this.stream = __libc__.popen(filename, mode);
-	}
-}
 
 print("stream 14");
