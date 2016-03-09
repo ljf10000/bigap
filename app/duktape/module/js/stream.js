141e818c5325069f75321de15b96b5aa3f3fb84b
ljf10000 <l_j_f@163.com>
2016-03-09 17:53:19 +0800

    .

--- app/duktape/module/js/stream.js
+++ app/duktape/module/js/stream.js
@@ -6,7 +6,7 @@ this.end        = __libc__.SEEK_END;
 
 sb = true;
 
-this.Stream = function __Stream (filename, mode) {
+function Stream (filename, mode) {
 	var pipe        = arguments[2]?arguments[2]:false;
 
 	this.filename   = filename;
@@ -18,80 +18,4 @@ this.Stream = function __Stream (filename, mode) {
 	} else {
 		this.stream = __libc__.popen(filename, mode);
 	}
-};
-
-this.__ok = function () {
-	return typeof this.stream === 'pointer' && null !== this.stream
-};
-
-this.close = function () {
-	if (this.__ok()) {
-		if (this.pipe) {
-			__libc__.pclose(this.stream);
-		} else {
-			__libc__.fclose(this.stream);
-		}
-
-		this.stream = null;
-	}
-};
-
-Duktape.fin(__Stream.prototype, function (obj, heapDestruct) {
-	if (heapDestruct) {
-		obj.close();
-	}
-
-	if (obj === __Stream.prototype) {
-        return;  // called for the prototype itself
-    }
-
-	obj.close();
-});
-
-this.read = function (buffer, size) {
-	return __libc__.fread(buffer, size, 1, this.stream);
-};
-
-this.readEx = function (size) {
-	return __libc__.freadEx(this.stream, size);
-};
-
-this.write = function (buffer, size) {
-	if (this.pipe) {
-		return -(__libc__.ENOSUPPORT);
-	} else {
-		return __libc__.fwrite(buffer, size, 1, this.stream);
-	}
-};
-
-this.eof = function () {
-	return __libc__.feof(this.stream);
-};
-
-this.error = function () {
-	return __libc__.ferror(this.stream);
-};
-
-this.tell = function () {
-	if (this.pipe) {
-		return -(__libc__.ENOSUPPORT);
-	} else {
-		return __libc__.ftell(this.stream);
-	}
-};
-
-this.seek = function (offset, where) {
-	if (this.pipe) {
-		return -(__libc__.ENOSUPPORT);
-	} else {
-		return __libc__.ftell(this.stream, where);
-	}
-};
-
-this.flush = function () {
-	if (this.pipe) {
-		return -(__libc__.ENOSUPPORT);
-	} else {
-		return __libc__.fflush(this.stream);
-	}
-};
+}
