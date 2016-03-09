f64e3721ad78268a6db38c1e55ef4236adca38f3
ljf10000 <l_j_f@163.com>
2016-03-09 17:27:46 +0800

    .

--- app/duktape/module/js/stream.js
+++ app/duktape/module/js/stream.js
@@ -5,18 +5,4 @@ this.current    = __libc__.SEEK_CUR;
 this.begin      = __libc__.SEEK_SET;
 this.end        = __libc__.SEEK_END;
 
-print("stream 2");
-Stream = function (filename, mode, pipe = false) {
-	this.filename   = filename;
-	this.mode       = mode;
-	this.pipe       = pipe;
-
-	if (pipe) {
-		this.stream = __libc__.fopen(filename, mode);
-	} else {
-		this.stream = __libc__.popen(filename, mode);
-	}
-};
-
-
 print("stream 14");
