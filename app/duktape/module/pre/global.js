e35aaba94830bf19c569f5c5cbbaa76045e187dd
ljf10000 <l_j_f@163.com>
2016-03-10 17:54:08 +0800

    .

--- app/duktape/module/pre/global.js
+++ app/duktape/module/pre/global.js
@@ -4,7 +4,7 @@ function printo (name, obj) {
 	}
 }
 
-function debugger(level) {
+function __debugger(level) {
 	var newa = Array.prototype.slice.call(arguments);
 	
 	__my__.debugger(level, newa.slice(1).toString());
