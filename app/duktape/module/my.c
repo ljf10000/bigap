143581fc9fa6274abc84dbc6e350a9e08904f2f0
ljf10000 <l_j_f@163.com>
2016-03-12 01:58:25 +0800

    .

--- app/duktape/module/my.c
+++ app/duktape/module/my.c
@@ -659,13 +659,13 @@ env_register(duk_context *ctx)
 static void 
 arg_register(duk_context *ctx)
 {
-    int i;
+    int i, argc = __argc - 2;
     
     __set_obj_string(ctx, -1, "name", __argv[0]);
     __set_obj_string(ctx, -1, "script", __argv[1]);
 
     duk_push_array(ctx);
-    for (i=0; i<(__argc-2); i++) {
+    for (i=0; i<argc; i++) {
         __set_array_string(ctx, -1, i, __argv[2+i]);
     }
     duk_put_prop_string(ctx, -2, "argv");
