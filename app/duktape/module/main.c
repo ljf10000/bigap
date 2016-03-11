9f5dfefda01d39027521d2e4fa6c3c825811eca8
ljf10000 <l_j_f@163.com>
2016-03-11 15:33:49 +0800

    .

--- app/duktape/module/main.c
+++ app/duktape/module/main.c
@@ -8,21 +8,9 @@
 
 #include "utils.h"
 #include "dukc.h"
+#include "global.code"
 
 OS_INITER;
-
-#define duk_CODE                                                                \
-"function oprint (name, obj) {"                                         __crlf  \
-"    for (var p in obj) {"                                              __crlf  \
-"            print(name + '.' + p + ' = ' + obj[p]);"                   __crlf  \
-"    }"                                                                 __crlf  \
-"}"                                                                     __crlf  \
-                                                                                \
-"function debug(level) {"                                               __crlf  \
-"    var newa = Array.prototype.slice.call(arguments);"                 __crlf  \
-"    __my__.debug(level, newa.slice(1).toString());"                    __crlf  \
-"}"                                                                     __crlf  \
-/* end */
 
 duk_context *__ctx;
 int __argc;
