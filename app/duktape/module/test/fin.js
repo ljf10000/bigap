6f9a6724f4dc8a929cddf8de1cc8b926f46ce56d
ljf10000 <l_j_f@163.com>
2016-03-15 15:35:32 -0400

    .

--- app/duktape/module/test/fin.js
+++ app/duktape/module/test/fin.js
@@ -1,26 +1,13 @@
 #!/bin/js
 
-var a;
+var stream = require("stream");
+var file = new stream.Stream("fin.js", "r");
 
-function init() {
-    a = { foo: 123 };
+var obj = {};
+function close (obj) {
+	print('obj closed');
+};
 
-    Duktape.fin(a, function (x, heapDestruct) {
-	if (heapDestruct) {
-		print('hd:finalizer, foo ->', x.foo);
-		return;
-        }
-        try {
-            print('normal:finalizer, foo ->', x.foo);
-        } catch (e) {
-            print('bad:finalizer failed (ignoring): ' + e);
-        }
-    });
-}
-
-init();
-print('refcount finalizer');
-//a = null;
-print('mark-and-sweep finalizer')
-Duktape.gc();
+obj.name = 'my obj';
+__js__.objDestructor(obj, close);
 
