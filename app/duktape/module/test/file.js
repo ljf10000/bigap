516c4087f3571d191f87f1450e12d250846c3f6d
ljf10000 <l_j_f@163.com>
2016-03-15 15:35:27 -0400

    .

--- app/duktape/module/test/file.js
+++ app/duktape/module/test/file.js
@@ -1,14 +1,19 @@
 #!/bin/js
 
 var stream = require("stream");
+fmt.separator("stream");
 fmt.oprint("stream", stream);
 
 var file = new stream.Stream("file.js", "r");
 fmt.oprint("file", file);
 
 var buffer = file.readEx(10000);
-print("read buffer =", buffer.toString());
+fmt.separator("begin read buffer");
+print(buffer);
+fmt.separator("end read buffer");
 
-auto_load_test();
+fmt.oprint("__js__", __js__);
+
 debug(1, "sb", 1, 2, "3", file);
+auto_global();
 
