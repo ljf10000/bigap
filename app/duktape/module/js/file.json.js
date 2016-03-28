6e6dbb982b7ff280dc2263e10e10e037f410ee43
ljf10000 <l_j_f@163.com>
2016-03-28 17:38:57 +0800

    .

--- app/duktape/module/js/file.json.js
+++ app/duktape/module/js/file.json.js
@@ -8,19 +8,15 @@ var pt = mod.constructor.prototype;
 pt.$name = pt.$name || 'file.cache';
 pt.debugger = new ModDebugger(pt.$name);
 pt.load = function () {
-	var fobj = Object.create(pt, JSON.parse(__my__.readtxt(this.$filename)));
-	fobj.$name = this.$name;
-	fobj.$filename = this.$filename;
-
-	return fobj;
+	if (this.$filename) {
+		this.content = JSON.parse(__my__.readtxt(this.$filename));
+	}
 };
 
 pt.save = function () {
-	var obj = JSON.parse(JSON.stringify(this));
-	delete obj.$name;
-	delete obj.$filename;
-
-	__my__.writefile(this.$filename, JSON.stringify(obj));
+	if (this.$filename) {
+		__my__.writefile(this.$filename, JSON.stringify(this.content));
+	}
 };
 
 pt.bind = function (filename) {
