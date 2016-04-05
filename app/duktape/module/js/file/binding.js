6b2785fb0c13595cf5c9e1dba575a26629812839
ljf10000 <l_j_f@163.com>
2016-04-05 13:20:30 +0800

    .

--- app/duktape/module/js/file/binding.js
+++ app/duktape/module/js/file/binding.js
@@ -16,11 +16,11 @@ pt.load = function (obj) {
 		obj.content = obj.reader('');
 	}
 
-	print('load ' + obj.content);
+	pt.$debugger.trace('load ' + obj.content);
 };
 
 pt.save = function (obj, writer) {
-	print('save ' + obj.filename + ':' + obj.content);
+	pt.$debugger.trace('save ' + obj.filename + ':' + obj.content);
 
 	if (obj.filename && obj.content) {
 		__my__.writefile(obj.filename, obj.writer(obj.content));
