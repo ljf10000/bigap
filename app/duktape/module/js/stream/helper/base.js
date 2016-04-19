d7bb50eca9ea8c8f1ad1bd074b8d39b80a74c8cb
ljf10000 <l_j_f@163.com>
2016-04-19 13:29:18 +0800

    .

--- app/duktape/module/js/stream/helper/base.js
+++ app/duktape/module/js/stream/helper/base.js
@@ -7,16 +7,16 @@ print('base 1');
 var mod = this,
 	pt = mod.__proto__,
 	name = 'stream/helper/base';
-
+print('base 2');
 pt.$name = function () { return name; };
 pt.$debugger = new $Debugger(name);
-
+print('base 3');
 pt.is_open = function (obj) {
 	return typeof obj.stream === 'pointer' && obj.stream;
 };
-
+print('base 4');
 pt.is_close = function (obj) {
 	return null === obj.stream;
 };
-print('base 2');
+print('base 5');
 /* eof */
