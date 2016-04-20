3ab14aab3f6c41a1c799a6c923cedd47dc05a40d
ljf10000 <l_j_f@163.com>
2016-04-20 12:40:49 +0800

    .

--- app/duktape/module/js/stream/helper/base.js
+++ app/duktape/module/js/stream/helper/base.js
@@ -19,13 +19,20 @@ pt.is_close = function (obj) {
 };
 
 pt.stream = function (obj, name, filename, mode, type) {
+	print('base.stream 1');
 	var tmp_filename = maybe_string(filename);
+	print('base.stream 2');
 
 	obj.filename = tmp_filename;
+	print('base.stream 3');
 	obj.mode = maybe_string(mode) || 'r';
+	print('base.stream 4');
 	obj.type = maybe_string(type) || 'file';
+	print('base.stream 5');
 	obj.$name = function () { return name + '(' + tmp_filename + ')'; };
+	print('base.stream 6');
 	obj.stream = null;
+	print('base.stream 7');
 
 	return obj;
 };
