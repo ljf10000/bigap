9f4785e5a08a5f744786ceedef8b28e2102a9520
ljf10000 <l_j_f@163.com>
2016-04-21 17:05:02 +0800

    .

--- app/duktape/module/test/binding.js
+++ app/duktape/module/test/binding.js
@@ -6,8 +6,8 @@ var dname = 'test.direct.txt';
 
 var binding = require('file/binding');
 
-var j = binding.cache(jname, JSON.parse, JSON.stringify);
-var c = binding.cache(cname);
+var j = binding.cache(jname, JSON.parse, JSON.stringify); j.load();
+var c = binding.cache(cname); c.load();
 var d = binding.direct(dname);
 
 fmt.oprint(jname, j.content);
