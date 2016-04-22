980ba5e7f0a4c9f5a530f7274eb6eecb487e13f9
ljf10000 <l_j_f@163.com>
2016-04-22 10:29:46 +0800

    .

--- app/duktape/module/js/file/binding.js
+++ app/duktape/module/js/file/binding.js
@@ -9,7 +9,7 @@ var mod = this,
 mod.$name = function () { return name; };
 mod.$debugger = new $Debugger(name);
 
-mod.load = function (obj, reader) {
+function load (obj, reader) {
 	var safe_reader = maybe_function(reader) || allways;
 
 	if (obj.filename && __libc__.fexist(obj.filename)) {
@@ -21,44 +21,54 @@ mod.load = function (obj, reader) {
 	mod.$debugger.trace('load ' + obj.content);
 
 	return obj;
-};
+}
 
-mod.save = function (obj, writer) {
+function write (obj, append, writer) {
 	var safe_writer = maybe_function(writer) || allways_string;
 
 	if (obj.filename && obj.content) {
-		__my__.writefile(obj.filename, safe_writer(obj.content));
+		var writefile = append?__my__.appendfile:__my__.writefile;
+
+		writefile(obj.filename, safe_writer(obj.content));
 	}
 
-	mod.$debugger.trace('save ' + obj.filename + ':' + obj.content);
+	mod.$debugger.trace(append?'append ':'save ' + obj.filename + ':' + obj.content);
 
 	return obj;
-};
+}
 
-mod.bind = function (obj, filename, reader, writer) {
+function save (obj, writer) {
+	return write(obj, false, writer);
+}
+
+function append (obj, writer) {
+	return write(obj, true, writer);
+}
+
+function bind (obj, filename, reader, writer) {
 	obj.filename = maybe_string(filename);
 	obj.content = (reader && writer)?{}:'';
 	obj.$name = function() { return name + '(' + obj.filename + ')'; };
 
 	return obj;
-};
+}
 
 mod.cache = function (filename, reader, writer) {
-	return Object.setPrototypeOf(mod.bind({}, filename, reader, writer),{
+	return Object.setPrototypeOf(bind({}, filename, reader, writer),{
 		load: function () {
-			mod.load(this, reader);
+			load(this, reader);
 		},
 		save: function () {
-			mod.save(this, writer);
+			save(this, writer);
 		}
 	});
 };
 
 mod.direct = function (filename, reader, writer) {
-	return new Proxy(mod.bind({}, filename, reader, writer), {
+	return new Proxy(bind({}, filename, reader, writer), {
 		get: function (obj, key) {
 			if (key==='content') {
-				mod.load(obj, reader);
+				load(obj, reader);
 			}
 
 			return obj[key];
@@ -68,17 +78,29 @@ mod.direct = function (filename, reader, writer) {
 			obj[key] = value;
 
 			if (key==='content') {
-				mod.save(obj, writer);
+				save(obj, writer);
 			}
 		}
 	});
 };
 
-mod.object = function (filename, is_direct, reader, writer) {
-	if (is_direct) {
-		return mod.direct(filename, reader, writer);
-	} else {
-		return mod.cache(filename, reader, writer);
+mod.append = function (filename, reader, writer) {
+	return new Proxy(bind({}, filename, reader, writer), {
+		set: function (obj, key, value) {
+			obj[key] = value;
+
+			if (key==='content') {
+				append(obj, writer);
+			}
+		}
+	});
+};
+
+mod.object = function (filename, type, reader, writer) {
+	switch(type) {
+		case 'direct': return mod.direct(filename, reader, writer);
+		case 'append': return mod.append(filename, reader, writer);
+		case 'cache': default: return mod.cache(filename, reader, writer);
 	}
 };
 
--- app/duktape/module/test/binding.js
+++ app/duktape/module/test/binding.js
@@ -4,23 +4,29 @@ var binding = require('file/binding');
 var name, f;
 
 name = 'test.json';
-f = binding.object(name, false, JSON.parse, JSON.stringify);
+f = binding.object(name, 'cache', JSON.parse, JSON.stringify);
 f.load();
 f.content.age = 30; f.save(); print(name, ':', __my__.readtxt(name));
 f.content.age = 40; f.save(); print(name, ':', __my__.readtxt(name));
 f.content.age = 50; f.save(); print(name, ':', __my__.readtxt(name));
 
 name = 'test.cache.txt';
-f = binding.object(name);
+f = binding.object(name, 'cache');
 f.load();
 f.content = 30; f.save(); print(name, ':', __my__.readtxt(name));
 f.content = 40; f.save(); print(name, ':', __my__.readtxt(name));
 f.content = 50; f.save(); print(name, ':', __my__.readtxt(name));
 
 name = 'test.direct.txt';
-f = binding.object(name, true);
+f = binding.object(name, 'direct');
 f.content = 30; print(name, ':', __my__.readtxt(name));
 f.content = 40; print(name, ':', __my__.readtxt(name));
 f.content = 50; print(name, ':', __my__.readtxt(name));
+
+name = 'test.append.txt';
+f = binding.object(name, 'append', JSON.parse, JSON.stringify);
+f.content.age = 30; print(name, ':', __my__.readtxt(name));
+f.content.age = 40; print(name, ':', __my__.readtxt(name));
+f.content.age = 50; print(name, ':', __my__.readtxt(name));
 
 /* eof */
