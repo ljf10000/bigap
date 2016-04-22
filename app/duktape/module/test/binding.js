d5c74ee9f44437e2e1d5392eaf392b33aeaab500
ljf10000 <l_j_f@163.com>
2016-04-22 09:50:41 +0800

    .

--- app/duktape/module/test/binding.js
+++ app/duktape/module/test/binding.js
@@ -1,17 +1,26 @@
 #!/bin/js
 
-var jname = 'test.json';
-var cname = 'test.cache.txt';
-var dname = 'test.direct.txt';
-
 var binding = require('file/binding');
+var name, f;
 
-var j = binding.object(jname, false, JSON.parse, JSON.stringify); j.load();
-var c = binding.object(cname); c.load();
-var d = binding.object(dname, true);
+name = 'test.json';
+f = binding.object(name, false, JSON.parse, JSON.stringify);
+f.load();
+f.content.age = 30; f.save(); fmt.oprint(name, f.content);
+f.content.age = 40; f.save(); fmt.oprint(name, f.content);
+f.content.age = 50; f.save(); fmt.oprint(name, f.content);
 
-fmt.oprint(jname, j.content);
-print(cname, c.content);
-print(dname, d.content);
+name = 'test.cache.txt';
+f = binding.object(name);
+f.load();
+f.content = 30; f.save(); print(name, f.content);
+f.content = 40; f.save(); print(name, f.content);
+f.content = 50; f.save(); print(name, f.content);
+
+name = 'test.direct.txt';
+f = binding.object(name, true);
+f.content = 30; print(name, f.content);
+f.content = 40; print(name, f.content);
+f.content = 50; print(name, f.content);
 
 /* eof */
