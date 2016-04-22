638124cce468c156678018785b7506145e6d6546
ljf10000 <l_j_f@163.com>
2016-04-22 09:58:31 +0800

    .

--- app/duktape/module/test/binding.js
+++ app/duktape/module/test/binding.js
@@ -6,24 +6,21 @@ var name, f;
 name = 'test.json';
 f = binding.object(name, false, JSON.parse, JSON.stringify);
 f.load();
-f.content.age = 30; f.save(); fmt.oprint(name, f.content);
-f.content.age = 40; f.save(); fmt.oprint(name, f.content);
-f.content.age = 50; f.save(); fmt.oprint(name, f.content);
-print(name, ':', __my__.readtxt(name));
+f.content.age = 30; f.save(); print(name, ':', __my__.readtxt(name));
+f.content.age = 40; f.save(); print(name, ':', __my__.readtxt(name));
+f.content.age = 50; f.save(); print(name, ':', __my__.readtxt(name));
 
 name = 'test.cache.txt';
 f = binding.object(name);
 f.load();
-f.content = 30; f.save(); print(name, f.content);
-f.content = 40; f.save(); print(name, f.content);
-f.content = 50; f.save(); print(name, f.content);
-print(name, ':', __my__.readtxt(name));
+f.content = 30; f.save(); print(name, ':', __my__.readtxt(name));
+f.content = 40; f.save(); print(name, ':', __my__.readtxt(name));
+f.content = 50; f.save(); print(name, ':', __my__.readtxt(name));
 
 name = 'test.direct.txt';
 f = binding.object(name, true);
-f.content = 30; print(name, f.content);
-f.content = 40; print(name, f.content);
-f.content = 50; print(name, f.content);
-print(name, ':', __my__.readtxt(name));
+f.content = 30; print(name, ':', __my__.readtxt(name));
+f.content = 40; print(name, ':', __my__.readtxt(name));
+f.content = 50; print(name, ':', __my__.readtxt(name));
 
 /* eof */
