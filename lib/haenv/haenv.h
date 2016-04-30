99437d685e897d427f09efadeeb8901cf17f6188
ljf10000 <l_j_f@163.com>
2016-05-01 04:58:43 +0800

    .

--- lib/haenv/haenv.h
+++ lib/haenv/haenv.h
@@ -519,7 +519,7 @@ haee_md5(haenv_t *env, haenv_entry_t *e)
         haee_key(e), 
         haee_value(e));
 #if HAENV_DPRINT
-    os_dump_line(0, e->md5, 16, NULL);
+    __os_dump_buffer(e, 16, NULL);
 #endif
 }
 
@@ -539,8 +539,8 @@ __is_good_haee_md5(haenv_t *env, haenv_entry_t *e)
     haenv_debug("new md5");
     os_dump_line(0, md5, 16, NULL);
     
-    haenv_debug("old md5");
-    os_dump_line(0, e->md5, 16, NULL);
+    haenv_debug("entry");
+    __os_dump_buffer(e, 16, NULL);
 #endif
     
     return md5_eq(md5, e->md5);
