47aa7641b6d4230e89ab8273251f9e797c5f4584
ljf10000 <l_j_f@163.com>
2016-05-30 16:34:58 +0800

    .

--- app/sys/sys.c
+++ app/sys/sys.c
@@ -10,7 +10,7 @@ Copyright (c) 2016-2018, Supper Wali Technology. All rights reserved.
 #include "fcookie/fcookie.h"
 
 #if !(IS_PRODUCT_LTEFI_MD2 || IS_PRODUCT_LTEFI_MD3 || IS_PRODUCT_PC)
-#error "just support ltefi(v2/v3) md/pc board"
+#error "just support ltefi md2/md3/pc board"
 #endif
 
 OS_INITER;
