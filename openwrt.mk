dd9f248403a574cef732c365ed2fd91652ea14da
unknown <l_j_f@163.com>
2016-06-30 13:07:48 +0800

    .

--- openwrt.mk
+++ openwrt.mk
@@ -42,7 +42,7 @@ define Package/ak
   SECTION:=apps
   CATEGORY:=bigap
   TITLE:=SuperWalle Basic App
-  DEPENDS:=+libjson +libubox
+  DEPENDS:=+json-c +libubox
 endef
 
 define Package/ak/install
@@ -169,7 +169,7 @@ define Package/duktape
   SECTION:=apps
   CATEGORY:=bigap
   TITLE:=SuperWalle Basic App
-  DEPENDS:=+libjson +libubox
+  DEPENDS:=+json-c +libubox
 endef
 
 define Package/duktape/install
