cdeb9d51c41d00519bc5d89f6c82f28ed83ca5e6
ljf10000 <l_j_f@163.com>
2016-07-18 20:16:52 +0800

    .

--- app/hisi/gpio.c
+++ app/hisi/gpio.c
@@ -4,7 +4,7 @@ Copyright (c) 2016-2018, Supper Wali Technology. All rights reserved.
 #if IS_PRODUCT_LTEFI_MD
 
 static int
-clock_usage(void)
+gpio_help(int argc, char *argv[])
 {
 #define USAGE_MODULE    __THIS_APPNAME " clock"
     os_eprintln(USAGE_MODULE "show");
@@ -16,27 +16,24 @@ clock_usage(void)
     return -EFORMAT;
 }
 
-static cmd_table_t cmd_gpio[] = {
-    {
-        .argc   = 1,
-        .argv   = {"show"},
-        .handle = cmd_clock_show,
-    },
-    {
-        .argc   = 2,
-        .argv   = {"save", "auto"},
-        .handle = cmd_clock_save_auto,
-    },
-    {
-        .argc   = 1,
-        .argv   = {"save"},
-        .handle = cmd_clock_save_manual,
-    },
-    {
-        .argc   = 1,
-        .argv   = {"sync"},
-        .handle = cmd_clock_sync,
-    },
+static cmd_op_t gpio_op = {
+    .help   = gpio_help,
+    .showall= gpio_showall,
+    .getter = gpio_getter,
+    .setter = gpio_setter,
 };
+
+static int
+gpio_getsetter(int argc, char *argv[])
+{
+    return cmd_getsetter(argc, argv, &gpio_op);
+}
+
+static int
+gpio_main(int argc, char *argv[])
+{
+    return os_call(hisi_gpio_init, hisi_gpio_fini, gpio_getsetter, argc, argv);
+}
+
 #endif /* IS_PRODUCT_LTEFI_MD */
 
