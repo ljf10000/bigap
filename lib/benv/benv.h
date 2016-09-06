d1440537323febe87d43a6a692d34722f7929555
ljf10000 <l_j_f@163.com>
2016-09-06 22:12:20 +0800

    dutape-1.5.1

--- lib/benv/benv.h
+++ lib/benv/benv.h
@@ -2317,17 +2317,6 @@ static inline int benv_save_nothing(void) {return 0;}
 static inline int
 benv_save(void)
 {
-    int i;
-    os_println("os/current=%d, mark's crc=0x%x", __benv_current, benv_mark_crc(1));
-    
-    uint32 current = __benv_current;
-
-    for (i=0; i<BENV_BLOCK_COUNT; i++) {
-        __benv_current = i;
-        os_println("set os/current=%d, calc crc=0x%x", __benv_current, benv_block_crc(1));
-    }
-    __benv_current = current;
-    
     /*
     * 1. save blocks before mark
     * 2. save blocks after mark
