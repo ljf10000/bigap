2496968620c14bf07b578d436cbbc2c3b1b2d3da
ljf10000 <l_j_f@163.com>
2016-11-02 21:44:17 +0800

    .

--- app/duktape/module/test/loop.js
+++ app/duktape/module/test/loop.js
@@ -1,52 +1,58 @@
 #!/bin/js
 
-var loop = {
-	inotify: {
-		count: 0,
-		handle: function (ev) {
-			print("inotify times", loop.inotify.count++);
+var inotify = {
+	count: 0,
+	handle: function (ev) {
+		print("inotify times", loop.inotify.count++);
 
-			fmt.oprint(ev);
-		},
-		param: [
-			{
-				path: "/home/liujingfei/work/big",
-				mask: __libc__.IN_OPEN | __libc__.IN_ACCESS
-			}
-		]
+		fmt.oprint(ev);
 	},
-	signal: {
-		count: 0,
-		handle: function (siginfo) {
-		    print("signal times", loop.signal.count++);
+	param: [
+		{
+			path: "/home/liujingfei/work/big",
+			mask: __libc__.IN_OPEN | __libc__.IN_ACCESS
+		}
+	]
+};
 
-		    fmt.oprint(siginfo);
-		},
-		param: [
-			__libc__.SIGUSR1,
-			__libc__.SIGUSR2,
-			__libc__.SIGINT,
-			__libc__.SIGHUP,
-			__libc__.SIGTERM,
-			__libc__.SIGABRT,
-			__libc__.SIGBUS,
-			__libc__.SIGILL,
-			__libc__.SIGFPE,
-			__libc__.SIGSEGV
-		]
+var signal = {
+	count: 0,
+	handle: function (siginfo) {
+	    print("signal times", loop.signal.count++);
+
+	    fmt.oprint(siginfo);
 	},
-	timer: {
-		count: 0,
-		handle: function (times) {
-			print("timer times:", loop.timer.count++);
-		},
-		param: {
-			interval:{
-				sec:5,
-				nsec:0
-			}
+	param: [
+		__libc__.SIGUSR1,
+		__libc__.SIGUSR2,
+		__libc__.SIGINT,
+		__libc__.SIGHUP,
+		__libc__.SIGTERM,
+		__libc__.SIGABRT,
+		__libc__.SIGBUS,
+		__libc__.SIGILL,
+		__libc__.SIGFPE,
+		__libc__.SIGSEGV
+	]
+};
+
+var timer = {
+	count: 0,
+	handle: function (times) {
+		print("timer times:", loop.timer.count++);
+	},
+	param: {
+		interval:{
+			sec:5,
+			nsec:0
 		}
 	}
+};
+
+var loop = {
+	inotify: inotify,
+	signal: signal,
+	timer: timer
 };
 
 __my__.loop(loop);
