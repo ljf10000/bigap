c42c512877fcc3a3f0cf011e112476332a4470b2
ljf10000 <l_j_f@163.com>
2016-11-02 09:02:08 +0800

    .

--- test/loops/loops.c
+++ test/loops/loops.c
@@ -33,7 +33,7 @@ static struct {
 
 static loop_t loop = LOOP_INITER;
 
-static int timer_cb(uint32 times)
+static int timer_cb(loop_watcher_t *watcher, uint32 times)
 {
     os_println("timer %u", times);
 
