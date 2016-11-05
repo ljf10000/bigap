97f8567ecc7cdb8583b57fd8b50d529c43386d97
ljf10000 <l_j_f@163.com>
2016-11-05 18:42:06 +0800

    .

--- test/loopc/loopc.c
+++ test/loopc/loopc.c
@@ -64,7 +64,7 @@ client(int type, char *path, char *msg)
             loopc.buf[len] = 0;
             os_println("%s", loopc.buf);
 
-            os_println("short");
+            os_println("err=%d, errno=%d", len, -errno);
             return 0;
         }
         else if (0==len) {
