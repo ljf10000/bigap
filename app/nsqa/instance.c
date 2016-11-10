592bccba3061545a17b52dbfebf6d043678731b7
ljf10000 <l_j_f@163.com>
2016-11-10 22:50:13 +0800

    .

--- app/nsqa/instance.c
+++ app/nsqa/instance.c
@@ -58,7 +58,7 @@ __create(char *name, jobj_t jobj)
     instance->topic     = os_strdup(jobj_get_string(jval));
     
     jval = jobj_get(jobj, NSQ_INSTANCE_PORT_NAME);
-    int port = jobj_get_bool(jobj, jval);
+    int port = jobj_get_bool(jval);
     sockaddr_in_t *server  = &instance->addr.in;
     server->sin_family  = AF_INET;
     server->sin_port    = htons(port);
