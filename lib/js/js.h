cf48f3f6a53be17b7534abd8a2425b206a4f7021
ljf10000 <l_j_f@163.com>
2016-11-12 11:33:01 +0800

    .

--- lib/js/js.h
+++ lib/js/js.h
@@ -162,7 +162,7 @@ typedef struct {
 
 #define JS_PARAM_VERSION_DEFT       1
 #define JS_PARAM_NUMBER(x)          duke_##x##_param_number
-#define JS_PARAM_VERSION(x)         duke_##x##_version
+#define JS_PARAM_VERSION(x)         duke_##x##_param_version
 #define JS_PARAM_API(x, n, v)       enum{ JS_PARAM_NUMBER(x) = n, JS_PARAM_VERSION(x) = v }
 #define JS_PARAM(x, n)              JS_PARAM_API(x, n, JS_PARAM_VERSION_DEFT)
 #define JS_FUNC(x)                  {#x, duke_##x, JS_PARAM_NUMBER(x), JS_PARAM_VERSION(x)}
