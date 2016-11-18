151f0d93621ed2d5621efe06ea1aa3e10e27d4b4
ljf10000 <l_j_f@163.com>
2016-11-18 20:18:14 +0800

    .

--- app/umd/user.c
+++ app/umd/user.c
@@ -357,9 +357,9 @@ wan_offline(struct um_user *user)
     *      and keep others
     */
     __online_downtime(user, um_flow_type_wan)   = time(NULL);
-    __online_downtime(user, um_flow_type_lan)   = 0;
+    // __online_downtime(user, um_flow_type_lan)   = 0;
     
-    // debug_event("user %s wan offline", os_macstring(user->mac));
+    debug_event("user %s wan offline", os_macstring(user->mac));
 }
 
 STATIC struct um_user *
