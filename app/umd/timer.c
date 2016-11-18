85824346aaaf8a887f139928399eb7927cfcafc5
ljf10000 <l_j_f@163.com>
2016-11-18 19:52:24 +0800

    .

--- app/umd/timer.c
+++ app/umd/timer.c
@@ -183,8 +183,8 @@ STATIC void
 timer_handle(struct um_user *user, time_t now)
 {
     static um_timer_handle_t *handler[] = {
-        fake_timeout,
-        online_reauth,
+        //fake_timeout,
+        //online_reauth,
         online_timeout,
         online_aging,
         
