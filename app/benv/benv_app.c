7d7aa5a10673a23e64c7209355b347b732a618f8
ljf10000 <l_j_f@163.com>
2016-12-01 23:05:00 +0800

    .

--- app/benv/benv_app.c
+++ app/benv/benv_app.c
@@ -2,7 +2,6 @@
 Copyright (c) 2016-2018, Supper Walle Technology. All rights reserved.
 *******************************************************************************/
 OS_INITER;
-BENV_MAIN_INITER;
 
 int allinone_main(int argc, char *argv[])
 {
