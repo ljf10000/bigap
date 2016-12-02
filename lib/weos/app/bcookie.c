26abaa5273e0cbdee58c4b659d6785e73ea0697b
ljf10000 <l_j_f@163.com>
2016-12-02 21:29:06 +0800

    .

---
+++ lib/weos/app/bcookie.c
@@ -0,0 +1,141 @@
+/*******************************************************************************
+Copyright (c) 2016-2018, Supper Walle Technology. All rights reserved.
+*******************************************************************************/
+#if PRODUCT_BCOOKIE_ENABLE
+#ifdef __APP__
+STATIC int
+__bcookie_check(char *file, uint32 fsize, struct bcookie *obj, uint32 osize)
+{
+    if (NULL==file) {
+        return os_assertV(-EINVAL8);
+    }
+    else if (NULL==obj) {
+        return os_assertV(-EINVAL7);
+    }
+    else if (osize<=sizeof(struct bcookie)) {
+        return os_assertV(-EINVAL5);
+    }
+    else if (fsize<osize) {
+        return os_assertV(-EINVAL4);
+    }
+    else {
+        return 0;
+    }
+}
+
+STATIC int
+__bcookie_load(char *file, int fsize, struct bcookie *obj, int osize)
+{
+    int fd = INVALID_FD, err = 0, offset = INVALID_VALUE;
+    int id = BCOOKIE_ID(obj);
+    byte *mem = NULL;
+
+    err = __bcookie_check(file, fsize, obj, osize);
+    if (err<0) {
+        return err;
+    }
+
+    debug_trace("before open %s", file);
+    
+    fd = __os_file_lock(file, O_RDONLY, S_IRUSR | S_IWUSR, true);
+    if (false==is_good_fd(fd)) {
+        err = -errno; goto error;
+    }
+    
+    mem = (byte *)os_malloc(fsize);
+    if (NULL==mem) {
+        err = -ENOMEM; goto error;
+    }
+
+    if (fsize != read(fd, mem, fsize)) {
+        debug_error("read boot error:%d", -errno);
+        
+        err = -errno; goto error;
+    }
+    
+    offset = bcookie_find(mem, fsize, id);
+    if (offset<0) { /* yes, <0 */
+        err = offset; goto error;
+    }
+
+    os_memcpy(obj, mem + offset, osize);
+    
+error:
+    __os_file_unlock(fd);
+    if (mem) {
+        os_free(mem);
+    }
+    
+    return err;
+}
+
+STATIC int
+__bcookie_save(char *file, int fsize, struct bcookie *obj, int osize)
+{
+    int fd = INVALID_FD, err = 0, offset = INVALID_VALUE;
+    int id = BCOOKIE_ID(obj);
+    byte *mem = NULL;
+    
+    err = __bcookie_check(file, fsize, obj, osize);
+    if (err<0) {
+        return err;
+    }
+    
+    fd = __os_file_lock(file, O_RDWR, S_IRUSR | S_IWUSR, true);
+    if (false==is_good_fd(fd)) {
+        err = -errno; goto error;
+    }
+    
+    mem = (byte *)os_malloc(fsize);
+    if (NULL==mem) {
+        err = -ENOMEM; goto error;
+    }
+    
+    if (fsize != read(fd, mem, fsize)) {
+        debug_error("read boot error:%d", -errno);
+        
+        err = -errno; goto error;
+    }
+    
+    offset = bcookie_find(mem, fsize, id);
+    if (offset<0) { /* yes, <0 */
+        err = offset; goto error;
+    }
+    
+    err = lseek(fd, offset, SEEK_SET);
+    if (err < 0) {        /* <0 is error */
+        debug_error("seek boot error:%d", -errno);
+
+        err = -errno; goto error;
+    }
+    
+    if (osize != write(fd, obj, osize)) {
+        debug_error("write boot error:%d", -errno);
+
+        err = -errno; goto error;
+    }
+    
+error:
+    __os_file_unlock(fd);
+    if (mem) {
+        os_free(mem);
+    }
+    
+    return err;
+}
+
+int
+bcookie_load(struct bcookie *obj, int size)
+{
+    return __bcookie_load(PRODUCT_DEV_BOOT, PRODUCT_BOOT_SIZE, obj, size);
+}
+
+int
+bcookie_save(struct bcookie *obj, int size)
+{
+    return __bcookie_save(PRODUCT_DEV_BOOT, PRODUCT_BOOT_SIZE, obj, size);
+}
+
+#endif
+#endif
+/******************************************************************************/
---
+++ lib/weos/boot/bcookie.c
@@ -0,0 +1,159 @@
+/*******************************************************************************
+Copyright (c) 2016-2018, Supper Walle Technology. All rights reserved.
+*******************************************************************************/
+void
+bcookie_fake(struct bcookie *bc)
+{
+    bc->id[BCOOKIE_COUNT/2]++;
+    bc->id[BCOOKIE_COUNT/2]--;
+}
+
+void
+bcookie_cid_dump(uint32 cid[4])
+{
+    int i;
+
+    for (i=0; i<4; i++) {
+        os_println("CID[%d]:%d", i, cid[i]);
+    }
+
+    os_println("MID:%d", bcookie_cid_mid(cid));
+    os_println("PSN:%d", bcookie_cid_psn(cid));
+}
+
+int
+bcookie_find(byte *mem, int size, int id)
+{
+    struct bcookie bc = BCOOKIE(id);
+    if (size < sizeof(bc)) {
+        return -ETOOSMALL;
+    }
+    
+    byte *p = (byte *)os_memmem(mem, size, &bc, sizeof(bc));
+    if (NULL==p) {
+        debug_error("no-found bcookie:%d", id);
+        
+        return -ENOEXIST;
+    }
+
+    return p - mem;
+}
+
+#if PRODUCT_BCOOKIE_ENABLE
+#ifdef __BOOT__
+STATIC int
+__bcookie_check(uint32 begin, uint32 size, struct bcookie *obj, uint32 osize)
+{
+    if (osize<=sizeof(struct bcookie)) {
+        return os_assertV(-EINVAL5);
+    }
+    else if (size<osize) {
+        return os_assertV(-EINVAL4);
+    }
+    else {
+        return 0;
+    }
+}
+
+STATIC int
+__bcookie_load(int begin, int size, struct bcookie *obj, int osize)
+{
+    int err = 0, offset = INVALID_VALUE;
+    int id = BCOOKIE_ID(obj);
+    byte *mem = NULL;
+
+    err = __bcookie_check(begin, size, obj, osize);
+    if (err<0) {
+        return err;
+    }
+
+    mem = (byte *)os_malloc(size);
+    if (NULL==mem) {
+        err = -ENOMEM; goto error;
+    }
+
+    if (size != benv_emmc_read(begin, mem, size)) {
+        debug_error("read emmc error:%d", -errno);
+        
+        err = -errno; goto error;
+    }
+    
+    offset = bcookie_find(mem, size, id);
+    if (offset<0) { /* yes, <0 */
+        err = offset; goto error;
+    }
+
+    os_memcpy(obj, mem + offset, osize);
+    
+error:
+    if (mem) {
+        os_free(mem);
+    }
+    
+    return err;
+}
+
+STATIC int
+__bcookie_save(int begin, int size, struct bcookie *obj, int osize)
+{
+    int err = 0, offset = INVALID_VALUE;
+    int id = BCOOKIE_ID(obj);
+    byte *mem = NULL;
+    
+    err = __bcookie_check(begin, size, obj, osize);
+    if (err<0) {
+        return err;
+    }
+
+    mem = (byte *)os_malloc(size);
+    if (NULL==mem) {
+        err = -ENOMEM; goto error;
+    }
+    
+    if (size != benv_emmc_read(begin, mem, size)) {
+        os_println("read emmc error:%d", -errno);
+        
+        err = -errno; goto error;
+    }
+    
+    offset = bcookie_find(mem, size, id);
+    if (offset<0) { /* yes, <0 */
+        err = offset; goto error;
+    }
+    os_memcpy(mem + offset, obj, osize);
+
+    uint32 block_begin = os_align_down(offset, BENV_BLOCK_SIZE)/BENV_BLOCK_SIZE;
+    uint32 block_end   = OS_ALIGN(offset + osize - 1, BENV_BLOCK_SIZE)/BENV_BLOCK_SIZE;
+    uint32 block_count = block_end - block_begin + 1;
+    
+    if (block_count * BENV_BLOCK_SIZE != benv_emmc_write(begin + block_begin * BENV_BLOCK_SIZE, 
+                    mem + block_begin * BENV_BLOCK_SIZE, 
+                    block_count * BENV_BLOCK_SIZE)) {
+        os_println("write emmc error:%d", -errno);
+
+        err = -errno; goto error;
+    }
+    
+error:
+    if (mem) {
+        os_free(mem);
+    }
+    
+    return err;
+}
+
+int
+bcookie_load(struct bcookie *obj, int size)
+{
+    return __bcookie_load(0, PRODUCT_BOOT_SIZE, obj, size);
+}
+
+int
+bcookie_save(struct bcookie *obj, int size)
+{
+    return __bcookie_save(0, PRODUCT_BOOT_SIZE, obj, size);
+}
+
+#endif
+#endif
+/******************************************************************************/
