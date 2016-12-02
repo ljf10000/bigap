f3959e45d9bf208bcc606b9585a986e8a6a31d3d
ljf10000 <l_j_f@163.com>
2016-12-03 00:33:36 +0800

    .

--- lib/utils/dlist.h
+++ lib/utils/dlist.h
@@ -16,6 +16,8 @@ typedef struct dlist_s {
     uint32 count;
 } dlist_t;
 
+#define dlist_entry(_node, _type, _member)  container_of(_node, _type, _member)
+
 #define DLIST_INITER(_list)      {  \
     .count = 0,                     \
     .head  = LIST_HEAD_INIT(_list), \
@@ -27,263 +29,47 @@ typedef mv_t dlist_foreach_f(dlist_node_t *node);
 #endif
 /******************************************************************************/
 #if 2
-static inline dlist_node_t *
-____dlist_first(dlist_t *list)
-{
-    return list_entry(list->head.next, dlist_node_t, node);
-}
+EXTERN bool
+in_dlist(dlist_t *list, dlist_node_t *node);
 
-static inline dlist_node_t *
-____dlist_last(dlist_t *list)
-{
-    return list_entry(list->head.prev, dlist_node_t, node);
-}
+EXTERN bool
+is_dlist_empty(dlist_t *list);
 
-static inline dlist_node_t *
-____dlist_next(dlist_node_t *node)
-{
-    return list_entry(node->node.next, dlist_node_t, node);
-}
+EXTERN bool
+is_dlist_first(dlist_t *list, dlist_node_t *node);
 
-static inline dlist_node_t *
-____dlist_prev(dlist_node_t *node)
-{
-    return list_entry(node->node.prev, dlist_node_t, node);
-}
+EXTERN bool
+is_dlist_last(dlist_t *list, dlist_node_t *node);
 
-static inline bool
-__in_dlist(dlist_node_t *node)
-{
-    return NULL!=node->list;
-}
+EXTERN void
+dlist_init(dlist_t *list);
 
-static inline bool
-__is_dlist_empty(dlist_t *list)
-{
-    return list_empty(&list->head) && 0==list->count;
-}
+EXTERN dlist_node_t *
+dlist_first(dlist_t *list);
 
-static inline bool
-__is_dlist_first(dlist_node_t *node)
-{
-    return __in_dlist(node) && is_list_first(&node->node, &node->list->head);
-}
+EXTERN dlist_node_t *
+dlist_last(dlist_t *list);
 
-static inline bool
-__is_dlist_last(dlist_node_t *node)
-{
-    return __in_dlist(node) && is_list_last(&node->node, &node->list->head);
-}
+EXTERN dlist_node_t *
+dlist_next(dlist_t *list, dlist_node_t *node);
 
-static inline dlist_node_t *
-__dlist_first(dlist_t *list)
-{
-    return __is_dlist_empty(list)?NULL:____dlist_first(list);
-}
+EXTERN dlist_node_t *
+dlist_prev(dlist_t *list, dlist_node_t *node);
 
-static inline dlist_node_t *
-__dlist_last(dlist_t *list)
-{
-    return __is_dlist_empty(list)?NULL:____dlist_last(list);
-}
+EXTERN int
+dlist_del(dlist_t *list, dlist_node_t *node);
 
-static inline dlist_node_t *
-__dlist_next(dlist_node_t *node)
-{
-    return __is_dlist_last(node)?NULL:____dlist_next(node);
-}
+EXTERN int
+dlist_add(dlist_t *list, dlist_node_t *node);
 
-static inline dlist_node_t *
-__dlist_prev(dlist_node_t *node)
-{
-    return __is_dlist_first(node)?NULL:____dlist_prev(node);
-}
+EXTERN int
+dlist_add_tail(dlist_t *list, dlist_node_t *node);
 
-#define dlist_entry(_node, _type, _member)        container_of(_node, _type, _member)
-#define __dlist_first_entry(_list, _type, _member)   ({ \
-    dlist_node_t *__node = __dlist_first(_list);        \
-    __node?dlist_entry(__node, _type, _member):NULL;    \
-})
-#define __dlist_last_entry(_list, _type, _member)    ({ \
-    dlist_node_t *__node = __dlist_last(_list);         \
-    __node?dlist_entry(__node, _type, _member):NULL;    \
-})
-#define __dlist_next_entry(_pos, _member)            ({ \
-    dlist_node_t *__node = __dlist_next(&_pos->_member);\
-    __node?dlist_entry(__node, typeof(*(_pos)), _member):NULL; \
-})
-#define __dlist_prev_entry(_pos, _member)            ({ \
-    dlist_node_t *__node = __dlist_prev(&_pos->_member);\
-    __node?dlist_entry(__node, typeof(*(_pos)), _member):NULL; \
-})
+EXTERN void
+dlist_replace(dlist_node_t *old, dlist_node_t *new);
 
-static inline void
-__dlist_del(dlist_node_t *node)
-{
-    if (__in_dlist(node)) {
-        __list_del_entry(&node->node);
-        node->node.prev = NULL;
-        node->node.next = NULL;
-        node->list->count--;
-        node->list = NULL;
-    }
-}
-
-static inline void
-__dlist_add(dlist_t *list, dlist_node_t *node)
-{
-    if (false==__in_dlist(node)) {
-        list_add(&node->node, &list->head);
-
-        node->list = list;
-        list->count++;
-    }
-}
-
-static inline void
-__dlist_add_tail(dlist_t *list, dlist_node_t *node)
-{
-    if (false==__in_dlist(node)) {
-        list_add_tail(&node->node, &list->head);
-
-        node->list = list;
-        list->count++;
-    }
-}
-
-static inline void
-__dlist_replace(dlist_node_t *old, dlist_node_t *new)
-{
-    list_replace(&old->node, &new->node);
-    
-    os_swap_value(old->list, new->list);
-}
-
-static inline void
-__dlist_replace_init(dlist_node_t *old, dlist_node_t *new)
-{
-    list_replace_init(&old->node, &new->node);
-    
-    os_swap_value(old->list, new->list);
-}
-#endif
-/******************************************************************************/
-#if 3
-static inline bool
-in_dlist(dlist_t *list, dlist_node_t *node)
-{
-    return list && node && list==node->list && __in_dlist(node);
-}
-
-static inline bool
-is_dlist_empty(dlist_t *list)
-{
-    return NULL==list || __is_dlist_empty(list);
-}
-
-static inline bool
-is_dlist_first(dlist_t *list, dlist_node_t *node)
-{
-    return in_dlist(list, node) && __is_dlist_first(node);
-}
-
-static inline bool
-is_dlist_last(dlist_t *list, dlist_node_t *node)
-{
-    return in_dlist(list, node) && __is_dlist_last(node);
-}
-
-static inline void
-dlist_init(dlist_t *list)
-{
-    list->count = 0;
-    
-    INIT_LIST_HEAD(&list->head);
-}
-
-static inline dlist_node_t *
-dlist_first(dlist_t *list)
-{
-    return list?__dlist_first(list):NULL;
-}
-
-static inline dlist_node_t *
-dlist_last(dlist_t *list)
-{
-    return list?__dlist_last(list):NULL;
-}
-
-static inline dlist_node_t *
-dlist_next(dlist_t *list, dlist_node_t *node)
-{
-    return in_dlist(list, node)?__dlist_next(node):NULL;
-}
-
-static inline dlist_node_t *
-dlist_prev(dlist_t *list, dlist_node_t *node)
-{
-    return in_dlist(list, node)?__dlist_prev(node):NULL;
-}
-
-static inline int
-dlist_del(dlist_t *list, dlist_node_t *node)
-{
-    if (in_dlist(list, node)) {
-        __dlist_del(node);
-
-        return 0;
-    } else {
-        return -ENOINLIST;
-    }
-}
-
-static inline int
-dlist_add(dlist_t *list, dlist_node_t *node)
-{
-    if (list && node) {
-        if (__in_dlist(node)) {
-            __dlist_del(node);
-        }
-
-        __dlist_add(list, node);
-
-        return 0;
-    } else {
-        return -EKEYNULL;
-    }
-}
-
-static inline int
-dlist_add_tail(dlist_t *list, dlist_node_t *node)
-{
-    if (list && node) {
-        if (__in_dlist(node)) {
-            __dlist_del(node);
-        }
-
-        __dlist_add_tail(list, node);
-
-        return 0;
-    } else {
-        return -EKEYNULL;
-    }
-}
-
-static inline void
-dlist_replace(dlist_node_t *old, dlist_node_t *new)
-{
-    if (old && new) {
-        __dlist_replace(old, new);
-    }
-}
-
-static inline void
-dlist_replace_init(dlist_node_t *old, dlist_node_t *new)
-{
-    if (old && new) {
-        __dlist_replace_init(old, new);
-    }
-}
+EXTERN void
+dlist_replace_init(dlist_node_t *old, dlist_node_t *new);
 
 #define dlist_first_entry(_list, _type, _member)   ({   \
     dlist_node_t *__node = dlist_first(_list);          \
@@ -320,37 +106,11 @@ dlist_replace_init(dlist_node_t *old, dlist_node_t *new)
          _pos; \
          _pos=_tmp, _tmp=dlist_next_entry(_list, _tmp, _member))
 
-static inline dlist_node_t *
-dlist_find(dlist_t *list, dlist_eq_f *eq)
-{
-    if (list && eq) {
-        dlist_node_t *node;
-        
-        dlistForeach(list, node) {
-            if ((*eq)(node)) {
-                return node;
-            }
-        }
-    }
-    
-    return NULL;
-}
+EXTERN dlist_node_t *
+dlist_find(dlist_t *list, dlist_eq_f *eq);
 
-static inline int
-dlist_foreach(dlist_t *list, dlist_foreach_f *foreach)
-{
-    dlist_node_t *node, *tmp;
-    mv_u mv;
-    
-    dlistForeachSafe(list, node, tmp) {
-        mv.v = (*foreach)(node);
-        if (is_mv2_break(mv)) {
-            return mv2_error(mv);
-        }
-    }
-
-    return 0;
-}
+EXTERN int
+dlist_foreach(dlist_t *list, dlist_foreach_f *foreach);
 #endif
 
 #ifdef __BOOT__
--- lib/weos/boot/dlist.c
+++ lib/weos/boot/dlist.c
@@ -1,5 +1,273 @@
 /*******************************************************************************
 Copyright (c) 2016-2018, Supper Walle Technology. All rights reserved.
 *******************************************************************************/
+DECLARE dlist_node_t *
+____dlist_first(dlist_t *list)
+{
+    return list_entry(list->head.next, dlist_node_t, node);
+}
 
+DECLARE dlist_node_t *
+____dlist_last(dlist_t *list)
+{
+    return list_entry(list->head.prev, dlist_node_t, node);
+}
+
+DECLARE dlist_node_t *
+____dlist_next(dlist_node_t *node)
+{
+    return list_entry(node->node.next, dlist_node_t, node);
+}
+
+DECLARE dlist_node_t *
+____dlist_prev(dlist_node_t *node)
+{
+    return list_entry(node->node.prev, dlist_node_t, node);
+}
+
+DECLARE bool
+__in_dlist(dlist_node_t *node)
+{
+    return NULL!=node->list;
+}
+
+DECLARE bool
+__is_dlist_empty(dlist_t *list)
+{
+    return list_empty(&list->head) && 0==list->count;
+}
+
+DECLARE bool
+__is_dlist_first(dlist_node_t *node)
+{
+    return __in_dlist(node) && is_list_first(&node->node, &node->list->head);
+}
+
+DECLARE bool
+__is_dlist_last(dlist_node_t *node)
+{
+    return __in_dlist(node) && is_list_last(&node->node, &node->list->head);
+}
+
+DECLARE dlist_node_t *
+__dlist_first(dlist_t *list)
+{
+    return __is_dlist_empty(list)?NULL:____dlist_first(list);
+}
+
+DECLARE dlist_node_t *
+__dlist_last(dlist_t *list)
+{
+    return __is_dlist_empty(list)?NULL:____dlist_last(list);
+}
+
+DECLARE dlist_node_t *
+__dlist_next(dlist_node_t *node)
+{
+    return __is_dlist_last(node)?NULL:____dlist_next(node);
+}
+
+DECLARE dlist_node_t *
+__dlist_prev(dlist_node_t *node)
+{
+    return __is_dlist_first(node)?NULL:____dlist_prev(node);
+}
+
+DECLARE void
+__dlist_del(dlist_node_t *node)
+{
+    if (__in_dlist(node)) {
+        __list_del_entry(&node->node);
+        node->node.prev = NULL;
+        node->node.next = NULL;
+        node->list->count--;
+        node->list = NULL;
+    }
+}
+
+DECLARE void
+__dlist_add(dlist_t *list, dlist_node_t *node)
+{
+    if (false==__in_dlist(node)) {
+        list_add(&node->node, &list->head);
+
+        node->list = list;
+        list->count++;
+    }
+}
+
+DECLARE void
+__dlist_add_tail(dlist_t *list, dlist_node_t *node)
+{
+    if (false==__in_dlist(node)) {
+        list_add_tail(&node->node, &list->head);
+
+        node->list = list;
+        list->count++;
+    }
+}
+
+DECLARE void
+__dlist_replace(dlist_node_t *old, dlist_node_t *new)
+{
+    list_replace(&old->node, &new->node);
+    
+    os_swap_value(old->list, new->list);
+}
+
+DECLARE void
+__dlist_replace_init(dlist_node_t *old, dlist_node_t *new)
+{
+    list_replace_init(&old->node, &new->node);
+    
+    os_swap_value(old->list, new->list);
+}
+
+DECLARE bool
+in_dlist(dlist_t *list, dlist_node_t *node)
+{
+    return list && node && list==node->list && __in_dlist(node);
+}
+
+DECLARE bool
+is_dlist_empty(dlist_t *list)
+{
+    return NULL==list || __is_dlist_empty(list);
+}
+
+DECLARE bool
+is_dlist_first(dlist_t *list, dlist_node_t *node)
+{
+    return in_dlist(list, node) && __is_dlist_first(node);
+}
+
+DECLARE bool
+is_dlist_last(dlist_t *list, dlist_node_t *node)
+{
+    return in_dlist(list, node) && __is_dlist_last(node);
+}
+
+DECLARE void
+dlist_init(dlist_t *list)
+{
+    list->count = 0;
+    
+    INIT_LIST_HEAD(&list->head);
+}
+
+DECLARE dlist_node_t *
+dlist_first(dlist_t *list)
+{
+    return list?__dlist_first(list):NULL;
+}
+
+DECLARE dlist_node_t *
+dlist_last(dlist_t *list)
+{
+    return list?__dlist_last(list):NULL;
+}
+
+DECLARE dlist_node_t *
+dlist_next(dlist_t *list, dlist_node_t *node)
+{
+    return in_dlist(list, node)?__dlist_next(node):NULL;
+}
+
+DECLARE dlist_node_t *
+dlist_prev(dlist_t *list, dlist_node_t *node)
+{
+    return in_dlist(list, node)?__dlist_prev(node):NULL;
+}
+
+DECLARE int
+dlist_del(dlist_t *list, dlist_node_t *node)
+{
+    if (in_dlist(list, node)) {
+        __dlist_del(node);
+
+        return 0;
+    } else {
+        return -ENOINLIST;
+    }
+}
+
+DECLARE int
+dlist_add(dlist_t *list, dlist_node_t *node)
+{
+    if (list && node) {
+        if (__in_dlist(node)) {
+            __dlist_del(node);
+        }
+
+        __dlist_add(list, node);
+
+        return 0;
+    } else {
+        return -EKEYNULL;
+    }
+}
+
+DECLARE int
+dlist_add_tail(dlist_t *list, dlist_node_t *node)
+{
+    if (list && node) {
+        if (__in_dlist(node)) {
+            __dlist_del(node);
+        }
+
+        __dlist_add_tail(list, node);
+
+        return 0;
+    } else {
+        return -EKEYNULL;
+    }
+}
+
+DECLARE void
+dlist_replace(dlist_node_t *old, dlist_node_t *new)
+{
+    if (old && new) {
+        __dlist_replace(old, new);
+    }
+}
+
+DECLARE void
+dlist_replace_init(dlist_node_t *old, dlist_node_t *new)
+{
+    if (old && new) {
+        __dlist_replace_init(old, new);
+    }
+}
+
+DECLARE dlist_node_t *
+dlist_find(dlist_t *list, dlist_eq_f *eq)
+{
+    if (list && eq) {
+        dlist_node_t *node;
+        
+        dlistForeach(list, node) {
+            if ((*eq)(node)) {
+                return node;
+            }
+        }
+    }
+    
+    return NULL;
+}
+
+DECLARE int
+dlist_foreach(dlist_t *list, dlist_foreach_f *foreach)
+{
+    dlist_node_t *node, *tmp;
+    mv_u mv;
+    
+    dlistForeachSafe(list, node, tmp) {
+        mv.v = (*foreach)(node);
+        if (is_mv2_break(mv)) {
+            return mv2_error(mv);
+        }
+    }
+
+    return 0;
+}
 /******************************************************************************/
