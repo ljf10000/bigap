1bfba5a7ee1093f027cae789d73a08499a6b5ef0
ljf10000 <l_j_f@163.com>
2016-12-07 00:48:25 +0800

    .

--- lib/utils/cli.h
+++ lib/utils/cli.h
@@ -324,9 +324,9 @@ __clic_recv(int fd, int timeout)
             err = __io_recv(fd, __clib_buf, len, 0);
             if (err==len) {
                 // __clib_show();
-                os_printf("recv: ...");
+                os_printf("recv: ...\n");
                 os_printf("recv:%s", __clib_buf);
-                os_printf("recv: ok.");
+                os_printf("recv: ok.\n");
             } else {
                 goto error;
             }
