45274a02cb45c87380893264a34684b1628d5d2f
ljf10000 <l_j_f@163.com>
2016-12-29 10:05:48 +0800

    .

--- lib/utils/jscript.h
+++ lib/utils/jscript.h
@@ -73,7 +73,7 @@ if content:exist, filename:exist, then
     "exectime": "1900-01-01 00:00:00",
     "seq": SEQ-NUMBER,
     "id": "GUID",
-    "ack": "error/reply",
+    "type": "error/reply",
     "reply": {
         "stdout": "STDOUT",
         "stderr": "STDERR",
---
+++ test/crypt/crypt.c
@@ -0,0 +1,47 @@
+/*******************************************************************************
+Copyright (c) 2016-2018, Supper Walle Technology. All rights reserved.
+*******************************************************************************/
+#ifndef __THIS_APP
+#define __THIS_APP      crypt
+#endif
+
+#include "utils.h"
+
+OS_INITER;
+
+#define KEYSIZE 8
+#define KEYBITS (32*KEYSIZE)
+
+int main(int argc, char *argv[])
+{
+    byte key[32]        = "0123456789abcdef0123456789ABCDEF";
+    byte plaintext[16]  = "0123456789abcdef";
+    byte ciphertext[16];
+    uint32 key32[KEYSIZE];
+
+    aes_key_setup(key, key32, KEYBITS);
+
+    aes_encrypt(plaintext, ciphertext, key32, KEYBITS);
+    os_println("plaintext:");
+    os_dump_buffer(plaintext, sizeof(plaintext));
+    os_println("ciphertext:");
+    os_dump_buffer(ciphertext, sizeof(ciphertext));
+
+    aes_decrypt(ciphertext, plaintext, key32, KEYSIZE);
+    os_println("plaintext:");
+    os_dump_buffer(plaintext, sizeof(plaintext));
+    os_println("ciphertext:");
+    os_dump_buffer(ciphertext, sizeof(ciphertext));
+
+    byte hmac[32];
+
+    byte message[32] = "001122334455667788aabbccddeeff";
+    hmac_sha256(key, sizeof(key), message, sizeof(message), hmac);
+    os_println("message:");
+    os_dump_buffer(message, sizeof(message));
+    os_println("hmac:");
+    os_dump_buffer(hmac, sizeof(hmac));
+    
+    return 0;
+}
+/******************************************************************************/
