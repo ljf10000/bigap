8015725eb21c1304767d5d1bba2f8d66106422d7
ljf10000 <l_j_f@163.com>
2017-01-05 21:18:09 +0800

    .

--- app/rsha/instance.c
+++ app/rsha/instance.c
@@ -273,7 +273,8 @@ __rshi_add_watcher(rsh_instance_t *instance)
     os_closexec(fd);
     instance->fd = fd;
     debug_entry("instance %s setup socket %d ok", instance->sp, fd);
-    
+
+#if 0
     sockaddr_in_t client = OS_SOCKADDR_INET(INADDR_ANY, 0);
     err = bind(fd, (sockaddr_t *)&client, sizeof(client));
     if (err<0) {
@@ -281,6 +282,7 @@ __rshi_add_watcher(rsh_instance_t *instance)
         
         return -errno;
     }
+#endif
 
     err = os_loop_add_normal(&rsha.loop, fd, rsha_recver, instance);
     if (err<0) {
