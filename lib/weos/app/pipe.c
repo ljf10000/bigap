d7142a1d62c2ddc679cbc57e7ec48de3873add00
ljf10000 <l_j_f@163.com>
2017-01-13 00:38:06 +0800

    .

--- lib/weos/app/pipe.c
+++ lib/weos/app/pipe.c
@@ -302,9 +302,9 @@ os_pexecv(pipinfo_t *info)
         err = -errno;
     }
     else if (pid>0) { // father
-        os_println("os_pexecv father ...");
+        pipe_println("os_pexecv father ...");
         err = __pipe_father_handle(&pe, pid);
-        os_println("os_pexecv father error:%d", err);
+        pipe_println("os_pexecv father error:%d", err);
         if (0==err) {
             (*info->cb)(pe.err, pe.std[1].sb.buf, pe.std[2].sb.buf);
         }
